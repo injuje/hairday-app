d5113b052d0755011f3f775bdde1faf73fe5f146
injuje <jlvrs@cesar.school>
2026-04-26 00:43:34 -0300

    first commit

---
+++ src/modules/schedules/load.js
@@ -0,0 +1,15 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
+import { schedulesShow } from "../schedules/show.js";
+import { hoursLoad } from "../form/hours-load";
+
+const selectedDate = document.getElementById("date")
+
+export async function schedulesDay(){
+    const date = selectedDate.value
+
+    const dailySchedules = await scheduleFetchByDay({ date })
+
+    schedulesShow({ dailySchedules })
+
+    hoursLoad({ date, dailySchedules })
+}
